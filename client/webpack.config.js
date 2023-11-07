9e54065e1cab44a4d792c619a7d17c8c9fd8d589
L.J Solis <l.jsolis52@yahoo.com>
2023-11-06 22:18:42 -0600

    minor change in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -66,7 +66,7 @@ module.exports = () => {
           use: {
             loader: 'babel-loader',
             options: {
-              presents: ['@babel/preset-env'],
+              presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
